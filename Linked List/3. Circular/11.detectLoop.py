fd62ebff676c9062b437ad7c239c343feeda1f9f
NANIFLY31 <darkbutterflystar31@gmail.com>
2024-04-12 14:07:13 +0530

    Update 11.detectLoop.py

--- Linked List/3. Circular/11.detectLoop.py
+++ Linked List/3. Circular/11.detectLoop.py
@@ -16,34 +16,69 @@ class Node:
 class CircularLinkedList:
     def __init__(self):
         self.__head = None
+    
+    def get_head(self):
+        return self.__head
         
-    def insert_at_tail(self, data):
+    def push(self, data):
         p = Node()
         p.set_data(data)
         if self.__head == None:
             self.__head = p
             p.set_next(p)
-        else:           
+        else:
             current = self.__head
-            while True:
+            while current.get_next() != self.__head:
                 current = current.get_next()
-                if current.get_next() == self.__head:
-                    break
             current.set_next(p)
-            p.set_next(self.__head.get_next())
+            p.set_next(self.__head)
+
+        
+    def printList(self):
+        temp = self.__head
+        while True:
+            print(temp.get_data(), end=" ")
+            temp = temp.get_next()
+            if temp == self.__head:
+                break
+        print("\n")
+            
+    def detect_loop(self):
+        s = set()
+        t = 0
+        current = self.__head
+        while current:
+            if current in s:
+                t = 1
+                break
+            s.add(current)
+            current = current.get_next()
+        
+        print("loop found") if t else print("no loop")
+        
+  
 
 
 cll = CircularLinkedList()
+cll.push(10)
+cll.push(20)
+cll.push(30)
+cll.push(40)
+cll.push(50)
+
+# cll.get_head().get_next().get_next().get_next().set_next(cll.get_head())
 
 while True:
     print("---------------MENU-----------------")
-    print("1.insert")
-    print("2.setect loop")
+    print("1.print listinsert")
+    print("2.detect loop")
     op = int(input("enter your option: "))
     if op == 1:
-        data = int(input("enter the data: "))
-        cll.insert_at_tail(data)
+        print()
+        cll.printList()
     elif op == 2:
+        cll.detect_loop()
+    else:
         break
                 
         
