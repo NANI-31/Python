c8563963bce7c3afc363a52b81d1cd0fa7e91c0d
NANIFLY31 <darkbutterflystar31@gmail.com>
2024-04-12 07:05:48 +0530

    Update 1. Array.py

--- Stack/1. Array.py
+++ Stack/1. Array.py
@@ -1,28 +1,74 @@
-maxSize = 10
-top = -1
-stack = [0] * maxSize
-
-def isFull():
-    if top == maxSize:
-        return 1
-    else:
-        return 0
+class Satck:
+    def __init__(self):
+        self.__stack = []
+        self.__max_size = 0
     
-def push(data):
-    global top
-    if isFull() != 1:
-        top += 1
-        stack[top] = data
-    else:
-        print("Could not insert data, Stack is full.")
-    return data
+    def set_stack(self, data):
+        self.__stack.append(data)
+    def get_stack(self):
+        return self.__stack
+    
+    def set_maxsize(self, data):
+        self.__max_size = data
+    def get_maxsize(self):
+        return self.__max_size
+    
+    def is_empty(self):
+        return len(self.get_stack()) == 0
+    
+    def is_full(self):
+        if self.get_maxsize() is None:
+            return False
+        return len(self.get_stack() >= self.get_maxsize)
+    
+    def push(self, item):
+        if self.is_full():
+            print("stack overflow")
+            return
+        self.set_stack(item)
+        print("pushed", item, "into the stack")
+        
+    def pop(self):
+        if self.is_empty():
+            print("stack underflow")
+            return None
+        item = self.get_stack().pop()
+        print("popped", item, "from the stack")
+        return item
+    
+    def peek(self):
+        if self.is_empty():
+            print("stack is empty")
+            return None
+        return self.get_stack()[-1]
+    
+    def size(self):
+        return len(self.get_stack())
+    
 
-push(44)
-push(10)
-push(62)
-push(123)
-push(15)
-
-print("Stack Elements: ")
-for i in range(maxSize):
-    print(stack[i], end = " ")
+stack = Satck()
+print("set the size of stack")
+s_size = int(input())
+stack.set_maxsize(s_size)
+while True:
+    print("---------------MENU------------------")
+    print("1. push")
+    print("2. pop")
+    print("3. peek")
+    print("4. exit")
+    
+    op = input("enter your choice")
+    
+    if op == '1':
+        data = int(input())
+        stack.push(data)
+    elif op == '2':
+        data = stack.pop()
+    elif op == '3':
+        data = stack.peek()
+        if data is not None:
+            print("top element of the stack:", data)
+    elif op == '4':
+        break
+        
+    
